fd334680cf8674e4a639817ae5399c0fcc3b4bdc
fredyisasi <fredyisasi78@gmail.com>
2024-07-25 13:08:15 -0600

    git add

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,22 +2,20 @@
 import React from 'react';
 import '../pages/App.css';
 
-function Video({ videoId }) {
-  const videoSrc = `https://www.youtube.com/embed/${videoId}`;
-
+const Video = ({ videoId }) => {
   return (
-    <div className="video-responsive">
+    <div className="video">
       <iframe
         width="560"
         height="315"
-        src={videoSrc}
+        src={`https://www.youtube.com/embed/${videoId}`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
-        title="Embedded YouTube Video"
-      />
+        title="Embedded youtube"
+      ></iframe>
     </div>
   );
-}
+};
 
 export default Video;
--- src/pages/Tutorials.js
+++ src/pages/Tutorials.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -8,16 +8,45 @@ import Home from './Home';
 import News from './News';
 import Login from './Login';
 
-
 const Tutorials = () => {
+  const videoIds = [
+    "7bySeFzhJk8", // Reemplaza con tus IDs de video
+    "z03qvzKMwg4",
+    "7bySeFzhJk8",
+    "z03qvzKMwg4",
+    "7bySeFzhJk8", // Reemplaza con tus IDs de video
+    "z03qvzKMwg4",
+    "7bySeFzhJk8",
+    "z03qvzKMwg4",
+    "7bySeFzhJk8", // Reemplaza con tus IDs de video
+    "z03qvzKMwg4",
+    "7bySeFzhJk8",
+    "z03qvzKMwg4",
+  ];
+
+  const galleryRef = useRef(null);
+
+  const scrollLeft = () => {
+    galleryRef.current.scrollBy({ left: -300, behavior: 'smooth' });
+  };
+
+  const scrollRight = () => {
+    galleryRef.current.scrollBy({ left: 300, behavior: 'smooth' });
+  };
+
   return (
     <div>
-<h1>Curso de FOTOGRAFÍA BÁSICA : ISO, APERTURA Y VELOCIDAD </h1>
-    <div>
-      
-      <div className='video-container'>
-      <Video videoId="7bySeFzhJk8" /> {/* Reemplaza con el ID de tu video */}
-      </div>
+      <h1>Tutoriales de Fotografia Avanzada</h1>
+      <div className="carousel-container">
+        <button className="carousel-button left" onClick={scrollLeft}>{'<'}</button>
+        <div className="video-gallery" ref={galleryRef}>
+          {videoIds.map(videoId => (
+            <div className="video-container" key={videoId}>
+              <Video videoId={videoId} />
+            </div>
+          ))}
+        </div>
+        <button className="carousel-button right" onClick={scrollRight}>{'>'}</button>
       </div>
     </div>
   );
